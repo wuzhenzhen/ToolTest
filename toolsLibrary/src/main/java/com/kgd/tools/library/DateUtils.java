aa57f60fb78c1c8a453ff93c95113800266700eb
wuzhenzhen <kgd.zhen@gmail.com>
2019-04-24 11:53:38 +0800

    基础类

--- toolsLibrary/src/main/java/com/kgd/tools/library/DateUtils.java
+++ toolsLibrary/src/main/java/com/kgd/tools/library/DateUtils.java
@@ -11,6 +11,18 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
+/**
+ *   时间相关工具类
+ *
+ *   boolean getTwoHour(String st1, String st2)		二个时间间的差值,大于十分钟返回true否则返回false；
+ *	long getCurrentTimeByHHmm(String hhmm)	由 hhmm 获取 当前的 long 时间
+ *   addAndReduceMinute(String str, int value)	增加或减少几分钟 （如：str=09:50  value=-10 就是 09:40   value=10就是10:00）
+ *   isValidHHmm(String str)	检查  str 是否HH:mm格式
+ *   isLessThenCurrenTime   是否小于当前时间
+ *  getStringDateFromLong	Long时间格式化为指定String 格式 ，  （如 1547693420668/HH:mm 转换为10:50）
+ *	getYesterdayStartTime	返回昨天的开始时间 long （如 2019-04-24 00:00:00）
+ *	getYesterdayEndTime		返回昨天的结束时间 long （如 2019-04-24 23:59:59）
+ */
 public class DateUtils {
     private static String mYear;
     private static String mMonth;
@@ -123,6 +135,7 @@ public class DateUtils {
 		}
 	}
 
+	// 由 hhmm 获取 当前的 long 时间
 	public static long getCurrentTimeByHHmm(String hhmm){
         try {
             if(!isValidHHmm(hhmm)) return 0;
@@ -212,16 +225,46 @@ public class DateUtils {
 	}
 
 	/**
-	 *	Long时间格式化为指定String  如1547693420668  转为 10:50
-	 * @param date
-	 * @param format
-	 * @return
+	 *	Long时间格式化为指定String
+	 * @param date			如1547693420668
+	 * @param format		如HH:mm
+	 * @return				如转为 10:50
 	 */
 	public static String getStringDateFromLong(Long date, String format) {
 		SimpleDateFormat df = new SimpleDateFormat(format);
 		java.util.Date dt = new java.util.Date(date);
 		String sDateTime = df.format(dt);
 		return sDateTime;
+	}
+
+	/**
+	 *
+	 * @return  返回昨天的开始时间 long （如 2019-04-24 00:00:00）
+	 */
+	public static long getYesterdayStartTime(){
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH)-1);
+		calendar.set(Calendar.HOUR,0);
+		calendar.set(Calendar.MINUTE,0);
+		calendar.set(Calendar.SECOND,0);
+		calendar.set(Calendar.MILLISECOND,0);
+		System.out.println("开始时间："+calendar.getTime());
+		return calendar.getTimeInMillis();
+	}
+
+	/**
+	 *
+	 * @return  返回昨天的结束时间 long （如 2019-04-24 23:59:59）
+	 */
+	public static long getYesterdayEndTime(){
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH)-1);
+		calendar.set(Calendar.HOUR,23);
+		calendar.set(Calendar.MINUTE,59);
+		calendar.set(Calendar.SECOND,59);
+		calendar.set(Calendar.MILLISECOND,999);
+		System.out.println("结束时间："+calendar.getTime());
+		return calendar.getTimeInMillis();
 	}
 
 	public static void main(String[] args) {
